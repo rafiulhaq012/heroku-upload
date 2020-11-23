9acd5a2ae120a0cb019e792255bb51400175e066
Rafi <rafi@isystematic.com.pk>
2020-11-23 17:14:29 +0500

    .

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const port = process.env.PORT || 8080
 const app = express();
 
 app.use(compression())
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'build')));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
 
