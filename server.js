fa80be1cd10aedb49f3447594935f15226595c0f
Rafi <rafi@isystematic.com.pk>
2020-11-23 17:08:48 +0500

    .

---
+++ server.js
@@ -0,0 +1,21 @@
+"use strict";
+const path = require('path')
+var compression = require('compression')
+const http = require('http');
+const express = require('express');
+const port = process.env.PORT || 8080
+
+const app = express();
+
+app.use(compression())
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+})
+
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log('HTTP Server running on port ' + port);
+});
